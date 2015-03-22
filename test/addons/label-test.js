e2156e34d4cd1ab343e241c760c146c992aa3a43
alfredkam <akam@adcnx.com>
2015-03-22 04:27:57 -0700

    :art: Update addons/Label test

--- test/addons/label-test.js
+++ test/addons/label-test.js
@@ -1,110 +1,149 @@
-// TODO
-// rewrite test for label
+var chai = require('chai');
+var expect = chai.expect;
+var Label = require('../../addons/label');
 
-// var chai = require('chai');
-// var expect = chai.expect;
-// var Label = require('../../lib/utils/label');
+describe('addons/Label', function () {
+    var label;
+    var scale;
+    before(function () {
+        // mocks
+        var isArray = function (obj) {
+            return obj instanceof Array;
+        };
+        Label.append = function (parent, childs) {
+            if (parent === '') return childs;
 
-// describe('lib/utils/label', function () {
-//     var label;
-//     var scale;
-//     before(function () {
-//         label = new Label();
-//         scale = { min: { '0': 30, '1': 24 },
-//           max: { '0': 300, '1': 600 },
-//           maxSet: [],
-//           len: 10,
-//           rows: 2,
-//           ySecs: { 0: 3, 1: 3 },
-//           color: [ 'red', 'blue' ],
-//           scattered: false,
-//           line: true,
-//           'font-family': '"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif',
-//           height: 500,
-//           width: 1200,
-//           pHeight: 460,
-//           paddingY: 20,
-//           paddingX: 30 };
-//     });
+            if (!isArray(childs)) {
+              childs = [childs];
+            }
+            return parent.replace(/(.*)(<\/.*>$)/g, function (match, p1, p2) {
+                return p1 + childs.join("") + p2;
+            });
+        };
+        Label.make = function (tagName, attribute, dataAttribute, content) {
+            var el = '<' + tagName;
 
-//     it('describeYAxis (scale, multi : true ) should return a yaxis border', function () {
-//         var result = label.describeYAxis(scale, {multi: true});
-//         expect(result)
-//             .to.be.a('array')
-//             .to.have.length(2)
-//             .to.satisfy(function (arr) {
-//                 return /text/.test(arr[0]) && /text/.test(arr[1]);
-//             });
-//     });
+            if (tagName === 'svg') {
+                el += ' version="1.1" xmlns="http://www.w3.org/2000/svg"';
+            }
+            el += this._makePairs(attribute);
+            el += this._makePairs('data', dataAttribute);
+            return el += '>' + (content || content === 0 ? content : '') + '</'+tagName+'>';
+        };
+        Label._makePairs = function (prefix, json) {
+            if (arguments.length < 2) {
+              json = prefix;
+              prefix = '';
+            } else {
+              prefix += '-';
+            }
 
-//     it('describeYAxis (scale, true) should return a yaxis label set', function () {
-//         scale.ySecs = 3;
-//         scale.max = 300;
-//         var result = label.describeYAxis(scale, true);
-//         expect(result)
-//             .to.be.a('array')
-//             .to.have.length(1)
-//             .to.satisfy(function (arr) {
-//                 return /text/.test(arr[0]);
-//             });
-//     });
+            if (!json) return '';
 
-//     it('describeXAxis (scale, opts) should return a xaxis label set', function () {
-//         var scale = { min: { '0': 68, '1': 30 },
-//           max: { '0': 600, '1': 500 },
-//           maxSet: [],
-//           len: 10,
-//           rows: 2,
-//           ySecs: { '0': 3, '1': 5 },
-//           color: [ 'red', 'blue' ],
-//           scattered: false,
-//           line: true,
-//           'font-family': '"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif',
-//           height: 500,
-//           width: 1200,
-//           pHeight: 460,
-//           paddingY: 20,
-//           paddingX: 30,
-//           heightRatio: NaN,
-//           tickSize: 125.55556 };
+            var keys = Object.keys(json), len = keys.length;
+            var str = '';
+            while (len--) {
+              str += ' ' + prefix + keys[len] + '="' + json[keys[len]] + '"';
+            }
+            return str;
+        };
 
-//         var opts = { dateTimeLabelFormat: 'MM/DD hh ap',
-//           minUTC: 1378512000000,
-//           interval: '4h',
-//           format: 'dateTime' };
+        label = Label;
+        scale = { min: { '0': 30, '1': 24 },
+          max: { '0': 300, '1': 600 },
+          maxSet: [],
+          len: 10,
+          rows: 2,
+          ySecs: { 0: 3, 1: 3 },
+          color: [ 'red', 'blue' ],
+          scattered: false,
+          line: true,
+          'font-family': '"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif',
+          height: 500,
+          width: 1200,
+          pHeight: 460,
+          paddingY: 20,
+          paddingX: 30 };
+    });
 
-//         var result = label.describeXAxis(scale, opts);
-//         expect(result)
-//             .to.be.a('array')
-//             .to.have.length(1)
-//             .to.satisfy(function (arr) {
-//                 return /text/.test(arr[0]);
-//             });
-//     });
+    it('describeYAxis (scale, multi : true ) should return a yaxis border', function () {
+        var result = label.describeYAxis(scale, {multi: true});
+        expect(result)
+            .to.be.a('array')
+            .to.have.length(2)
+            .to.satisfy(function (arr) {
+                return /text/.test(arr[0]) && /text/.test(arr[1]);
+            });
+    });
 
-//     it('_utcMultiplier should return utc', function () {
-//         expect(label._utcMultiplier('1s'))
-//             .to.equal(1000);
-//         expect(label._utcMultiplier('1m'))
-//             .to.equal(6e4);
-//         expect(label._utcMultiplier('1h'))
-//             .to.equal(3600000);
-//         expect(label._utcMultiplier('1D'))
-//             .to.equal(86400000);
-//         expect(label._utcMultiplier('1M'))
-//             .to.equal(2592000000);
-//         expect(label._utcMultiplier('1Y'))
-//             .to.equal(31104000000);
-//     });
+    it('describeYAxis (scale, true) should return a yaxis label set', function () {
+        scale.ySecs = 3;
+        scale.max = 300;
+        var result = label.describeYAxis(scale, true);
+        expect(result)
+            .to.be.a('array')
+            .to.have.length(1)
+            .to.satisfy(function (arr) {
+                return /text/.test(arr[0]);
+            });
+    });
 
-//     it('_formatTimeStamp should return the desired time format', function () {
-//         var base = 1378512000000;
-//         expect(label._formatTimeStamp('YY/MM', base + 3600000))
-//             .to.equal('13/9');
-//         expect(label._formatTimeStamp('YYYY', base + 3600000))
-//             .to.equal('2013');
-//         expect(label._formatTimeStamp('mm.ss', base + 3600000))
-//             .to.equal('00.00');
-//     });
+    it('describeXAxis (scale, opts) should return a xaxis label set', function () {
+        var scale = { min: { '0': 68, '1': 30 },
+          max: { '0': 600, '1': 500 },
+          maxSet: [],
+          len: 10,
+          rows: 2,
+          ySecs: { '0': 3, '1': 5 },
+          color: [ 'red', 'blue' ],
+          scattered: false,
+          line: true,
+          'font-family': '"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif',
+          height: 500,
+          width: 1200,
+          pHeight: 460,
+          paddingY: 20,
+          paddingX: 30,
+          heightRatio: NaN,
+          tickSize: 125.55556 };
+
+        var opts = { dateTimeLabelFormat: 'MM/DD hh ap',
+          minUTC: 1378512000000,
+          interval: '4h',
+          format: 'dateTime' };
+
+        var result = label.describeXAxis(scale, opts);
+        expect(result)
+            .to.be.a('array')
+            .to.have.length(1)
+            .to.satisfy(function (arr) {
+                return /text/.test(arr[0]);
+            });
+    });
+
+    it('_utcMultiplier should return utc', function () {
+        expect(label._utcMultiplier('1s'))
+            .to.equal(1000);
+        expect(label._utcMultiplier('1m'))
+            .to.equal(6e4);
+        expect(label._utcMultiplier('1h'))
+            .to.equal(3600000);
+        expect(label._utcMultiplier('1D'))
+            .to.equal(86400000);
+        expect(label._utcMultiplier('1M'))
+            .to.equal(2592000000);
+        expect(label._utcMultiplier('1Y'))
+            .to.equal(31104000000);
+    });
+
+    it('_formatTimeStamp should return the desired time format', function () {
+        var base = 1378512000000;
+        expect(label._formatTimeStamp('YY/MM', base + 3600000))
+            .to.equal('13/9');
+        expect(label._formatTimeStamp('YYYY', base + 3600000))
+            .to.equal('2013');
+        expect(label._formatTimeStamp('mm.ss', base + 3600000))
+            .to.equal('00.00');
+    });
     
-// });
+});
