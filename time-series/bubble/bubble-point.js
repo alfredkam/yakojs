e883f1a7ce7601dbf73ce3260961e963deeeb113
alfredkam <akam@adcnx.com>
2015-04-25 15:38:51 -0700

    :art: Consolidate bubble graph codes

--- time-series/bubble/bubble-point.js
+++ time-series/bubble/bubble-point.js
@@ -1,5 +1,7 @@
 var Base = require('../../lib/base/default');
-var bubble = module.exports = Base.extend({
+var circle = require('../../lib/svg/circle');
+var bubble = require('../../lib/graphs/bubble');
+module.exports = Base.extend({
     // Start of a life cyle
     _startCycle: function () {
         var self = this;
@@ -35,79 +37,9 @@ var bubble = module.exports = Base.extend({
         return append(self.element, append(svg, paths));
     },
     // Extends default ratio w/ auto scaling
-    _getRatio: function (scale) {
-        var data = scale._data;
-        var height = scale.height;
-        var width = scale.width;
-        var len = scale.len;
-        var maxRadius = (height < width ? height : width) / 3;
-        var paddingRight = scale.paddingRight;
-        var paddingLeft = scale.paddingLeft;
-        var paddingTop = scale.paddingTop;
-        var paddingBottom = scale.paddingBottom;
-        scale.axis = scale.axis || {};
-        maxRadius = scale.maxRadius = parseInt(scale.maxRadius) || maxRadius;
-
-        // Determine if its time series
-        scale.timeSeries = true;
-        // Check if the start date is defined, if not defined using first element in array
-        // TODO:: Handle edge case of single point
-        scale.startTick = startTick = (scale.startDate || data[0].date).getTime();
-        scale.endTick = endTick = (scale.endDate || data[len - 1].date).getTime();
-        var tickLen = endTick - startTick;  // Need to handle zero
-        tickLen = (tickLen == 0 ? 1000 : tickLen);
-        var firstTick = data[0].date.getTime();
-        var lastTick = data[len - 1].date.getTime();
-        var firstTickLeftRadius = firstTick - startTick - (scale.maxRadius * data[0].data / scale.max);
-        var lastTickRightRadius = lastTick - endTick + (scale.maxRadius * data[len - 1].data / scale.max);
-        scale.paddingLeft = firstTickLeftRadius < 0 ? Math.abs(firstTickLeftRadius) : 0;
-        scale.paddingRight = lastTickRightRadius > 0 ? lastTickRightRadius : 0;
-        scale.tickSize = (width - scale.paddingLeft - scale.paddingRight) / (tickLen);
-    },
+    _getRatio: circle.getRatioByTimeSeries,
     // Describes the xAxis for bubble point graph
-    _describeXAxis: function (height, width, chart) {
-        var config = chart.axis;
-        var centerY = height / 2;
-        // Self Note:: PaddingLeft / PaddingRight adjustments are taken out
-        return this.make('path', {
-            "stroke-linecap": "round",
-            "stroke-linejoin": "round",
-            'stroke-width': config.strokeWidth || 2,
-            stroke: config.strokeColor || 'transparent',
-            d: 'M' + chart.paddingLeft + ' ' + centerY + ' H' + width
-        });
-    },
+    _describeXAxis: bubble.describeXAxisForBubbleLine,
     // Describes bubble point graph
-    _describeBubble: function (data, height, width, scale) {
-        if (!data) return '';
-        var dataPoints = data.length;
-        var paths = [];
-        var defaultStrokeColor = scale.strokeColors || 0;
-        var defaultStrokeWidth = scale.strokeWidths || 0;
-        var defaultFill = scale.fill || 0;
-        var centerY = height / 2;
-        var refs;
-        var tickSize = scale.tickSize;
-        var startTick = scale.startTick;
-
-        for (var i = 0; i < data.length; i++) {
-            var point = data[i];
-            if (scale.hasEvents) {
-                // c = columns
-                refs = {
-                    c: i
-                };
-            }
-            paths.push(this.make('circle', {
-                cx: ((point.date.getTime() - startTick) * tickSize) + scale.paddingLeft,
-                cy: centerY,
-                r: scale.maxRadius * point.data / scale.max,
-                fill: point.fill || defaultFill,
-                stroke: point.strokeColor || (defaultStrokeColor || 'transparent'),
-                'stroke-width' : point.strokeWidth || (defaultStrokeWidth || 0)
-            }, refs));
-        }
-
-        return paths;
-    }
+    _describeBubble: bubble.describeLineByObject
 });
--- time-series/bubble/bubble-scatter.js
+++ time-series/bubble/bubble-scatter.js
@@ -1,4 +1,6 @@
 var Base = require('../../lib/base/default');
+var circle = require('../../lib/svg/circle');
+var bubble = require('../../lib/graphs/bubble');
 module.exports = Base.extend({
     _startCycle: function () {
         var self = this;
@@ -30,70 +32,6 @@ module.exports = Base.extend({
     },
     // Describes bubble scattered graph
     // Extends default ratio w/ auto scaling
-    _getRatio: function (scale) {
-        var data = scale._data;
-        var height = scale.height;
-        var width = scale.width;
-        var len = scale.len;
-        var maxRadius = (height < width ? height : width) / 3;
-        var paddingRight = scale.paddingRight;
-        var paddingLeft = scale.paddingLeft;
-        var paddingTop = scale.paddingTop;
-        var paddingBottom = scale.paddingBottom;
-        // bubble as a scattered graph
-        maxRadius = scale.maxRadius = parseInt(scale.maxRadius) || Math.sqrt(width * height / len) / 2;
-        scale.paddingLeft = paddingLeft < maxRadius ? maxRadius : paddingLeft;
-        scale.paddingRight = paddingRight < maxRadius ? maxRadius : paddingRight;
-        scale.paddingTop = paddingTop < maxRadius ? maxRadius : paddingTop;
-        scale.paddingBottom = paddingBottom < maxRadius ? maxRadius : paddingBottom;
-        scale.widthRatio = (width - scale.paddingLeft - scale.paddingRight) / scale.max[0];
-        scale.heightRatio = (height - scale.paddingTop - scale.paddingBottom) / scale.max[1];
-    },
-    _describeBubbleChart: function(data, scale) {
-        var height = scale.height;
-        var width = scale.width;
-        var heightRatio = scale.heightRatio;
-        var widthRatio = scale.widthRatio;
-        var self = this;
-        var len = scale.len;
-        var max = scale.max;
-        var defaultFill = scale.fill || 0;
-        var defaultStrokeColor = scale.strokeColor || 0;
-        var defaultStrokeWidth = scale.strokeWidth || 0;
-        var paths = [];
-        var refs;
-
-        // Acceptable inverse flags to inverse the data set
-        var inverseList = {
-            'x': 'x',
-            'y': 'y'
-        };
-        var inverse = {};
-        if (scale.inverse) {
-            for (var x in scale.inverse) {
-                if (inverseList[scale.inverse[x]]) {
-                    inverse[inverseList[scale.inverse[x]]] = true;
-                }
-            }
-        }
-        scale.hasInverse = inverse;
-
-        for (var i = 0; i < len; i++) {
-            var props = data[i];
-            var point = props.data;
-            if (scale.hasEvents) {
-                // c = column for reference
-                refs = {
-                    c: i
-                };
-            }
-            paths.push(self.make('circle', {
-                cx: (inverse.x ? (point[0] * widthRatio) + scale.paddingLeft : width - (point[0] * widthRatio) - scale.paddingLeft),
-                cy: (inverse.y ? scale.paddingTop + (point[1] * heightRatio) : height - (point[1] * heightRatio) - scale.paddingTop),
-                r: scale.maxRadius * (point[2]/max[2]),
-                fill: props.fill || (defaultFill || self._randomColor())
-            }, refs));
-        }
-        return paths;
-    }
+    _getRatio: circle.getRatioByObject,
+    _describeBubbleChart: bubble.describeBubbleByObject
 });
